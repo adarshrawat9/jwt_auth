537021fce207b2b0f485b95db4b7dad36abb702f
adarshrawat9 <adarshrawat9368@gmail.com>
2026-06-04 18:48:00 +0530

    added Authenticate method

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,5 +1,38 @@
 package middleware
 
-import(
+import (
+	"fmt"
+	"jwt-auth/helper"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 )
+
+
+func Authenticate() gin.HandlerFunc{
+	return func (c *gin.Context)  {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error getting token")})
+			c.Abort()
+			return 
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return 
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_Name)
+		c.Set("last_name", claims.Last_Name)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.User_type)
+		c.Next()
+
+
+	}
+}
+
