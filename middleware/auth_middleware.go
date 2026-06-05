fc8fa1e4b8ec3164816966fde2dd68062d1374ad
adarshrawat9 <adarshrawat9368@gmail.com>
2026-06-05 19:40:16 +0530

    updated http status code for unauthorized access

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -13,14 +13,14 @@ func Authenticate() gin.HandlerFunc{
 	return func (c *gin.Context)  {
 		clientToken := c.Request.Header.Get("token")
 		if clientToken == ""{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error getting token")})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("token is not provided")})
 			c.Abort()
 			return 
 		}
 
 		claims, err := helper.ValidateToken(clientToken)
-		if err != ""{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		if err != nil{
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			c.Abort()
 			return 
 		}
